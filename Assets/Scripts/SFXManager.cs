194057de02d3ff3505396d3bc24a44c2469f8ed7
MatheusHL <matheuscamposhl@gmail.com>
2019-03-25 11:06:05 -0300

    aula 3

---
+++ Assets/Scripts/SFXManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    public static SFXManager instance;
+
+    public GameObject coinParticles;
+
+    void awake(){
+        if (instance == null){
+            instance = this;
+
+        }
+
+    }
+    public void ShowCoinParticles(GameObject obj){
+         GameObject particles = Instantiate(coinParticles, obj.transform.position, Quaternion.identity); 
+            GameObject Tape = GameObject.Find("Tape");
+            particles.transform.SetParent(Tape.transform);
+    }
+
+}
